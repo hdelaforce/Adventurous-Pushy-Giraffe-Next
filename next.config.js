74ee15ec38ae00bd51fe397391354ff28bfa3200
hudsondelaforce <hdela33@eq.edu.au>
2025-03-04 14:46:22 +1000

    trying to fix explosion.gif - permission policy

--- next.config.js
+++ next.config.js
@@ -17,3 +17,23 @@ module.exports = {
     ];
   },
 };
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://84de7b7d82bbe88b3d12d556c324ea34@o4508917644918784.ingest.de.sentry.io/4508917653307472",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById(“app”);
+const root = createRoot(container);
+root.render(<App />);
